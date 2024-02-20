91093f099cff642f46a74626da17128f618f2748
francescopennisi00 <pennisi.f@studium.unict.it>
2024-02-20 19:59:58 +0100

    added class files for worker

---
+++ worker_microservice/worker/utils/constants.py
@@ -0,0 +1,8 @@
+BOOTSTRAP_SERVER_KAFKA = 'kafka-service:9092'
+GROUP_ID = 'group1'
+TOPIC_NAME = "event_to_be_notified"
+TIMEOUT_POLL_REQUEST = 5.0
+ATTEMPTS = 5
+UM_ADDRESS = 'um-service:50051'
+SMTP_SERVER_NAME = 'smtp.gmail.com'
+SMTP_SERVER_PORT = 465
